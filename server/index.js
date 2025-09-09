51b3319e5180f6ccb1e30461844658d1ac2af70e
Suyash Tripathi <suyatripathi03@gmail.com>
2025-09-09 11:50:59 +0530

    url change

--- server/index.js
+++ server/index.js
@@ -13,8 +13,8 @@ const app = express();
 app.use(
   cors({
     // origin: "http://localhost:5173",
+    origin: "https://ai-assistant-smoky-kappa.vercel.app/",
     // origin: "https://ai-assistant-smoky-kappa.vercel.app",
-    origin: "https://ai-assistant-smoky-kappa.vercel.app",
     credentials: true,
   })
 );
