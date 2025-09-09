2b8295e364c9e3502e8b70108774ec928dca97cc
Suyash Tripathi <suyatripathi03@gmail.com>
2025-09-09 11:31:55 +0530

    fix

--- server/index.js
+++ server/index.js
@@ -12,8 +12,8 @@ const app = express();
 
 app.use(
   cors({
-    // origin: "http://localhost:5173",
-    origin: "https://ai-assistant-kvru.onrender.com",
+    origin: "http://localhost:5173",
+    // origin: "https://ai-assistant-kvru.onrender.com",
     credentials: true,
   })
 );
