f78825a4dd94d769cef9dffd74b0c1a58c12bc8c
Suyash Tripathi <suyatripathi03@gmail.com>
2025-09-16 00:28:54 +0530

    serverurl

--- client/src/context/UserContext.jsx
+++ client/src/context/UserContext.jsx
@@ -5,7 +5,7 @@ export const userDataContext = createContext();
 
 const UserContext = ({ children }) => {
   // const serverUrl = "http://localhost:8080";
-  const serverUrl = "https://myaiassistantbackend.onrender.com/";
+  const serverUrl = "https://myaiassistantbackend.onrender.com";
   const [userData, setUserData] = useState(null);
 
   // Conversation history from MySQL
