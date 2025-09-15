4928777c79705a1f41ba432b1152c5a9c301f016
Suyash Tripathi <suyatripathi03@gmail.com>
2025-09-15 23:29:20 +0530

    finalMysql update

--- server/config/mysql.js
+++ server/config/mysql.js
@@ -19,22 +19,15 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.MYSQL_DATABASE,   // e.g. ai_assistant
-  process.env.MYSQLUSER,        // Railway se milega (root nahi hamesha)
-  process.env.MYSQL_ROOT_PASSWORD,    // Railway se milega
-  {
-    host: process.env.MYSQLHOST, // e.g. containers-us-west-123.railway.app
-    port: process.env.MYSQLPORT || 3306, 
-    dialect: "mysql",
-    logging: false,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false, // Railway/PlanetScale ke liye SSL required
-      },
+const sequelize = new Sequelize(process.env.MYSQL_PUBLIC_URL, {
+  dialect: "mysql",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Railway/PlanetScale ke liye SSL required
     },
-  }
-);
+  },
+});
 
 export default sequelize;
