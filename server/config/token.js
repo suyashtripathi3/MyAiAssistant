7cc4a984e0cd915876c3208e10f122d94f4d2ec9
Suyash Tripathi <suyatripathi03@gmail.com>
2025-10-27 12:06:28 +0530

    tocken fix

--- server/config/token.js
+++ server/config/token.js
@@ -14,20 +14,12 @@ const genToken = (userId, res) => {
   const token = jwt.sign({ userId }, JWT_SECRET, { expiresIn: "7d" });
 
   // Set cookie
-<<<<<<< HEAD
   // genToken.js
   res.cookie("token", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
     secure: process.env.NODE_ENV === "development" ? false : true,
-=======
-  res.cookie("token", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-    httpOnly: true, // prevent XSS
-    sameSite: NODE_ENV === "development" ? "strict" : "none",
-    secure: NODE_ENV === "development" ? false : true, // secure cookies in production
->>>>>>> 31f34452e83e5af58dc750fd614b01e171a8fc1c
   });
 
   return token;
